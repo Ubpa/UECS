600a5cf3a3808da8350b3eded94a9dc07d2561d5
Ubpa <641614112@qq.com>
2020-03-04 15:50:17 +0800

    clear up test/00_basic

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # UECS
 Ubpa Entity-Component-System
+
+## Simple Example
+
+```c++
+#include <UECS/core/World.h>
+#include <iostream>
+
+struct velocity { float value{ 0.f }; };
+struct position { float value{ 0.f }; };
+
+int main() {
+	Ubpa::World w;
+
+	for (size_t i = 0; i < 10; i++) {
+		auto [entity, v, p] = w.CreateEntity<velocity, position>();
+		v->value = static_cast<float>(i);
+	}
+
+	float deltaT = 0.033f;
+
+	w.Each([deltaT](velocity* v, position* p) {
+		p->value += v->value * deltaT;
+		});
+
+	w.Each([](position* p) {
+		std::cout << p->value << std::endl;
+		});
+
+	return 0;
+}
+```
+
--- src/test/00_basic/main.cpp
+++ src/test/00_basic/main.cpp
@@ -1,64 +1,27 @@
 #include <UECS/core/World.h>
-
 #include <iostream>
-#include <set>
 
-using namespace std;
-using namespace Ubpa;
-
-struct velocity {
-	velocity() = default;
-	velocity(float v) :v(v) {};
-	float v{ 0.f };
-};
-
-struct position {
-	float x{ 0.f };
-};
+struct velocity { float value{ 0.f }; };
+struct position { float value{ 0.f }; };
 
 int main() {
-	World w;
-	set<Entity*> entities;
+	Ubpa::World w;
 
 	for (size_t i = 0; i < 10; i++) {
 		auto [entity, v, p] = w.CreateEntity<velocity, position>();
-		entities.insert(entity);
-		v->v = static_cast<float>(i);
+		v->value = static_cast<float>(i);
 	}
 
-	for (size_t i = 0; i < 5; i++) {
-		auto [entity, p] = w.CreateEntity<position>();
-		auto [v] = entity->Attach<velocity>();
-		entities.insert(entity);
-		v->v = 10 + static_cast<float>(i);
-	}
+	float deltaT = 0.033f;
 
-	for (size_t i = 0; i < 5; i++) {
-		auto [entity, p] = w.CreateEntity<position>();
-		entities.insert(entity);
-	}
-	
-	for (auto e : entities)
-		cout << e->Get<position>()->x << endl;
+	w.Each([deltaT](velocity* v, position* p) {
+		p->value += v->value * deltaT;
+		});
 
-	int i = 0;
-	auto sys = [&i](velocity* v, position* p) {
-		p->x += v->v * 0.01f;
-		cout << "i: " << i << endl;
-		i++;
-	};
-
-	w.Each(sys);
-
-	for (auto e : entities)
-		cout << e->Get<position>()->x << endl;
-
-	for (auto e : entities) {
-		if (e->Get<velocity>()) {
-			e->Detach<velocity>();
-			e->Attach<velocity>();
-		}
-	}
+	w.Each([](position* p) {
+		std::cout << p->value << std::endl;
+		});
 
 	return 0;
 }
+
