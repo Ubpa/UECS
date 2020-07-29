0749089d9295ae7519e842d48229c94298c900ad
Ubpa <641614112@qq.com>
2020-07-29 21:31:50 +0800

    add test performance

---
+++ src/test/13_performance/main.cpp
@@ -0,0 +1,42 @@
+#include <UECS/World.h>
+
+#include <chrono>
+#include <iostream>
+
+struct A { float val; };
+struct B { float val; };
+
+struct TestSystem {
+	static void OnUpdate(Ubpa::Schedule& schedule) {
+		schedule.Register(
+			[](const A* a, B* b) {
+				// 256 floating-point operations
+				for (size_t i = 0; i < 256; i++)
+					b->val *= a->val;
+			}, "TestSystem");
+	}
+};
+
+int main() {
+	size_t numEntities = 65536;
+	size_t numUpdate = 144 * 10;
+	Ubpa::World w;
+	w.systemMngr.Register<TestSystem>();
+
+	auto t0 = std::chrono::steady_clock::now();
+	for (size_t i = 0; i < numEntities; i++)
+		w.entityMngr.Create<A, B>();
+	auto t1 = std::chrono::steady_clock::now();
+	for (size_t i = 0; i < numUpdate; i++)
+		w.Update();
+	auto t2 = std::chrono::steady_clock::now();
+
+	// about 10s
+	// G5400 : 4 core
+	auto d0 = t1 - t0;
+	auto d1 = t2 - t1;
+	std::cout << "create: " << d0.count() / 1000000000.0 << "s" << std::endl;
+	std::cout << "update: " << d1.count() / 1000000000.0 << "s" << std::endl;
+
+	return 0;
+}
